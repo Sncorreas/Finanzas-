391d5788c05b085beed8be7cbb35aae51554c279
Sncorreas <sarahinicole@hotmail.com>
2025-07-24 10:26:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ window.onload = function() {
   // Por ejemplo, si quieres modificar el texto dinámicamente
   // subtitle.textContent = "✨ ¡Tú puedes con TODO! 🥺❤️ ✨";
 }
+window.onload = function() {
+  // Seleccionamos todos los items de materias
+  const materias = document.querySelectorAll('.semestre ul li');
+
+  materias.forEach(item => {
+    item.addEventListener('click', () => {
+      // Al hacer click, alternamos la clase 'tachado'
+      item.classList.toggle('tachado');
+    });
+  });
+}
