cd014eab2091c11b7c3cdccf39fd8330517bb142
Sncorreas <sarahinicole@hotmail.com>
2025-07-24 10:30:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-// Ejemplo sencillo: cambia el texto del h2 al cargar la página
 window.onload = function() {
-  const subtitle = document.querySelector('header h2');
-  // Por ejemplo, si quieres modificar el texto dinámicamente
-  // subtitle.textContent = "✨ ¡Tú puedes con TODO! 🥺❤️ ✨";
-}
-window.onload = function() {
-  // Seleccionamos todos los items de materias
   const materias = document.querySelectorAll('.semestre ul li');
 
   materias.forEach(item => {
     item.addEventListener('click', () => {
-      // Al hacer click, alternamos la clase 'tachado'
       item.classList.toggle('tachado');
     });
   });
